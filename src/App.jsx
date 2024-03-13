2b297fd219800817f1154d517d341134723d3474
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2024-03-13 16:39:55 +0600

    crud done for firebase todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useState } from "react";
-import { getDatabase, push, ref, set, onValue } from "firebase/database";
+import {
+  getDatabase,
+  push,
+  ref,
+  set,
+  onValue,
+  remove,
+  update,
+} from "firebase/database";
+
 import firebaseConfig from "./Configuration/FireBaseConf";
 
 function App() {
   const [Text, setText] = useState("");
   const [Todos, setTodos] = useState([]);
-
+  const [Edit, setEdit] = useState();
+  //calling data basee
   const db = getDatabase();
+
+  // submitting data
   const handleSubmit = () => {
     if (Text !== "") {
       set(push(ref(db, "alltodo")), {
@@ -19,23 +31,73 @@ function App() {
     }
   };
 
+  // reciving existing data or todo
   useEffect(() => {
     const starCountRef = ref(db, "alltodo");
     onValue(starCountRef, snapshot => {
-      const data = snapshot.val();
-      console.log(data);
-
-      const TodoArr = data ? Object.values(data) : [];
-      setTodos(TodoArr);
+      let todoArr = [];
+      snapshot.forEach(item => {
+        const data = item.val();
+        const id = item.key;
+        const todoItem = {
+          data: data,
+          id: id,
+        };
+        // pushing every todo and todo id to the todoArr
+        todoArr.push(todoItem);
+      });
+      // finally pushing data to todos
+      setTodos(todoArr);
     });
   }, []);
+
+  // getting update todo data and update id
+  const handleEditInfo = (updateId, data) => {
+    console.log(updateId);
+    if (updateId && data) {
+      setEdit(updateId);
+      setText(data);
+    }
+  };
+
+  // updating existing todo
+  const HandleEdit = () => {
+    const Mytodo = {
+      Mytodo: Text,
+    };
+    console.log(Edit);
+    update(ref(db, "alltodo/" + Edit), Mytodo).then(() => {
+      console.log("updated data");
+      setText("");
+      setEdit("");
+    });
+  };
+
+  // deleting existing todo
+  const handleDelete = deleteId => {
+    remove(ref(db, "alltodo/" + deleteId)).then(
+      console.log("sucessfully deleted")
+    );
+  };
 
   return (
     <>
       <input type="text" onChange={e => setText(e.target.value)} value={Text} />
-      <button onClick={handleSubmit}>Submit Data</button>
+      {Edit ? (
+        <button onClick={HandleEdit}> edit Data</button>
+      ) : (
+        <button onClick={handleSubmit}> submit Data</button>
+      )}
       {Todos.map((todo, index) => (
-        <p key={index}> {todo.Mytodo} </p>
+        <div key={index}>
+          {" "}
+          <p> {todo.data.Mytodo} </p>
+          <button onClick={() => handleDelete(todo.id)}> delete todo</button>
+          <button onClick={() => handleEditInfo(todo.id, todo.data.Mytodo)}>
+            {" "}
+            edit todo
+          </button>
+        </div>
       ))}
     </>
   );
